f5d6ec61599fd5245119d70d8815a250c36282d7
Maxim Maxim <apc-mv@yandex.ru>
2023-07-21 14:43:48 +0500

    add sorted from rules

--- src/componets/aside/aside.tsx
+++ src/componets/aside/aside.tsx
@@ -1,12 +1,21 @@
 import { Button } from "../button";
 import styles from "./aside.module.scss";
+import { useAppDispatch } from "../../redux/store/hooks";
+import { sortNameUser, sortCityUser } from "../../redux/slices/userSlices"
 
 export const Aside = () => {
+    const dispatch = useAppDispatch();
+    const sortByName = () => {
+        dispatch(sortNameUser());
+    }
+    const sortByCity = () => {
+        dispatch(sortCityUser())
+    }
     return (
         <div className={styles.item}>
             <p>Сортировка</p>
-            <Button color="purple">по городу</Button>
-            <Button color="purple">по компании</Button>
+            <Button onClick={sortByName} color="purple">по имени</Button>
+            <Button onClick={sortByCity} color="purple">по компании</Button>
         </div>
     );
 };
--- src/redux/slices/userSlices.tsx
+++ src/redux/slices/userSlices.tsx
@@ -13,16 +13,22 @@ export const Users = createSlice({
     initialState,
     reducers: {
         getUsers: (state, action: PayloadAction<IUsers>) => {
-            state.allUsers = action.payload
-            state.loading = false
+            state.allUsers = action.payload,
+                state.loading = false
         },
-        selectedUser: (state, action) => {
+        selectedUser: (state, action: PayloadAction<number>) => {
             state.allUsers = state.allUsers.filter(item => item.id === action.payload)
             console.log("данные которые приходят в старый слайс", action.payload)
+        },
+        sortNameUser: (state) => {
+            state.allUsers = state.allUsers.sort((a, b) => { if (a["name"] < b["name"]) return -1 });
+        },
+        sortCityUser: (state) => {
+            state.allUsers = state.allUsers.sort((a, b) => { if (a["address"].city < b["address"].city) return -1 });
         }
     },
 })
 
-export const { getUsers, selectedUser } = Users.actions
+export const { getUsers, selectedUser, sortNameUser, sortCityUser } = Users.actions
 
 export default Users.reducer
